aa87cacc608fd40e0781c30ebb09b2854ac07b4f
Aryan <aryanbisht9458@gmail.com>
2023-11-15 08:35:03 +0530

    Trip and payment Socket.

---
+++ lib/web_socket/trip_socket.dart
@@ -0,0 +1,39 @@
+import 'dart:convert';
+
+import 'package:flutter/material.dart';
+import 'package:myride/utils/utils.dart';
+import 'package:myride/view/for_driver/map_section/map_screen.dart';
+import 'package:web_socket_channel/web_socket_channel.dart';
+
+class TripWebSocket {
+  static WebSocketChannel? channel;
+
+  webSocketInit() {
+    debugPrint("Started");
+    channel = WebSocketChannel.connect(
+      Uri.parse('ws://3.109.183.75:7401/ws/trip-notify/96'),
+    );
+  }
+
+  void listenSocket(callbackMethod, context) {
+    debugPrint("Listen");
+    channel!.stream.listen((message) {
+      debugPrint("Received $message");
+      Map map = jsonDecode(message);
+      Utils.showMyDialog("Trip started", context);
+      Navigator.push(
+          context,
+          MaterialPageRoute(
+              builder: (context) => MapScreenDriver(
+                    map: map,
+                  )));
+    });
+  }
+
+  void _addMessage(String text, bool isMe) {
+    Map data = {
+      "message": text,
+    };
+    channel!.sink.add(json.encode(data));
+  }
+}
